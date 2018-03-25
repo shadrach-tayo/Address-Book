7fd7b4e7120933dcbee6c297884bf1b41d488652
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2018-03-26 00:55:49 +0100

    Added Details view

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,17 @@
 class AddressBook {
 	constructor(name, contacts) {
+		// firstclass props
 		this.name = name;
 		this.contacts = contacts;
 		this.contactData = new Map();
-		this.views = {
-			default: document.querySelector('.header'),
-			edit: 'active',
-			add: ''
+		this.state = {
+			showingDetails: false,
+			editingContact: false
 		}
-		// this.viewList = ['default', 'edit', 'add']
+		this.deletedContacts = new Map();
+		
+		// template views
 		this.contactListTemplate = document.querySelector('.contacts');
-		// this.contactViewButtons = document.querySelectorAll('button.js-view-contact');
 
 		this.populateData = this.populateData.bind(this);
 		this.currentView = this.currentView.bind(this);
@@ -18,6 +19,7 @@ class AddressBook {
 		this.viewContactDetails = this.viewContactDetails.bind(this);
 		this.getViewButtons = this.getViewButtons.bind(this);
 		this.lastInit = this.lastInit.bind(this);
+
 
 		this.populateData();
 		this.renderContacts();
@@ -51,6 +53,8 @@ class AddressBook {
 			if(this.contactData.has(key)) {
 				let contact = this.contactData.get(key);
 				console.log(contact);
+				App.view.showDetails(contact);
+				// View.showDetails(contact);
 				break;
 			}
 		}
@@ -93,19 +97,118 @@ class AddressBook {
 
 
 var contacts = [
-	{id: '11a', name: 'Adeokun Victory', phone: +23418933925, email: 'adeokun.victory@gmail.com', avatarUrl: './assets/images/avatar.jpg'},
-	{id: '12a', name: 'Kelvin Doe', phone: +23418933925, email: 'kelvindoe@gmail.com', avatarUrl: './assets/images/avatar.jpg'},
-	{id: '13a', name: 'Jemimah Samuel', phone: +23418933925, email: 'Jemimahsamuel@gmail.com', avatarUrl: './assets/images/avatar.jpg'},
-	{id: '14a', name: 'Duyile John', phone: +23418933925, email: 'duyilejohn@gmail.com', avatarUrl: './assets/images/avatar.jpg'},
-	{id: '15a', name: 'Shadrach Morgan', phone: +23418933925, email: 'shadrachmorgan@gmail.com', avatarUrl: './assets/images/avatar.jpg'},
-	{id: '16a', name: 'James Bond', phone: +23418933925, email: 'shadrachbond@gmail.com', avatarUrl: './assets/images/avatar.jpg'},
-	{id: '17a', name: 'Brandy Morgan', phone: +23418933925, email: 'brandymorgan@gmail.com', avatarUrl: './assets/images/avatar.jpg'},
-	{id: '18a', name: 'Zoey felix', phone: +23418933925, email: 'zoeyfelix@gmail.com', avatarUrl: './assets/images/avatar.jpg'},
-	{id: '19a', name: 'Chris Sean', phone: +23418933925, email: 'chrissean@gmail.com', avatarUrl: './assets/images/avatar.jpg'},
-	{id: '11b', name: 'Lydia Hallie', phone: +23418933925, email: 'lydiahallie@gmail.com', avatarUrl: './assets/images/avatar.jpg'}
+	{id: '11a', name: 'Adeokun Victory', phone: +23418933925, email: 'adeokun.victory@gmail.com', avatarUrl: './assets/images/avatar.jpg', backgroundUrl: '../images/avatar.jpg'},
+	{id: '12a', name: 'Kelvin Doe', phone: +23418933925, email: 'kelvindoe@gmail.com', avatarUrl: './assets/images/avatar.jpg', backgroundUrl: '../images/avatar.jpg'},
+	{id: '13a', name: 'Jemimah Samuel', phone: +23418933925, email: 'Jemimahsamuel@gmail.com', avatarUrl: './assets/images/avatar.jpg', backgroundUrl: '../images/avatar.jpg'},
+	{id: '14a', name: 'Duyile John', phone: +23418933925, email: 'duyilejohn@gmail.com', avatarUrl: './assets/images/avatar.jpg', backgroundUrl: '../images/avatar.jpg'},
+	{id: '15a', name: 'Shadrach Morgan', phone: +23418933925, phone2: +35982359852, email: 'shadrachmorgan@gmail.com', avatarUrl: './assets/images/avatar.jpg', backgroundUrl: '../images/avatar.jpg'},
+	{id: '16a', name: 'James Bond', phone: +23418933925, email: 'shadrachbond@gmail.com', avatarUrl: './assets/images/avatar.jpg', backgroundUrl: '../images/avatar.jpg'},
+	{id: '17a', name: 'Brandy Morgan', phone: +23418933925, email: 'brandymorgan@gmail.com', avatarUrl: './assets/images/avatar.jpg', backgroundUrl: '../images/avatar.jpg'},
+	{id: '18a', name: 'Zoey felix', phone: +23418933925, email: 'zoeyfelix@gmail.com', avatarUrl: './assets/images/avatar.jpg', backgroundUrl: '../images/avatar.jpg'},
+	{id: '19a', name: 'Chris Sean', phone: +23418933925, email: 'chrissean@gmail.com', avatarUrl: './assets/images/avatar.jpg', backgroundUrl: '../images/avatar.jpg'},
+	{id: '11b', name: 'Lydia Hallie', phone: +23418933925, email: 'lydiahallie@gmail.com', avatarUrl: './assets/images/avatar.jpg', backgroundUrl: '../images/avatar.jpg'}
 ];
 
+
+class View {
+	constructor() {
+		// get templates
+
+		this.defaultTemplate = document.querySelector('.contacts');
+		this.detailsTemplate =  document.querySelector('.contact-view');
+		this.editTemplate = '';
+
+		this.templates = [this.detailsTemplate]
+
+		this.UtilityButtons = [];
+
+
+		// bind methods 
+		this.showDetails = this.showDetails.bind(this);
+		this.clearTemplate = this.clearTemplate.bind(this);
+		this.hideTemplates = this.hideTemplates.bind(this);
+		this.getUtilButtons = this.getUtilButtons.bind(this);
+		this.removeTemplate = this.removeTemplate.bind(this);
+		this.attachListenerAndCallback = this.attachListenerAndCallback.bind(this);
+
+	}
+
+	attachListenerAndCallback(elem, event, callback) {
+		elem.addEventListener(event, callback.bind(null, elem));
+		console.log(elem);
+	}
+
+	showDefault() {
+		this.defaultTemplate.style.display = 'block';
+	}
+
+	showDetails(contact) {
+		this.hideTemplates();
+		this.clearTemplate(this.detailsTemplate);
+		this.detailsTemplate.innerHTML += `
+			<div class="details-header" style="background-image: url(${contact.backgroundUrl})">
+				<span class="back-btn js-remove-template"></span>
+				<div class="action-buttons">
+					<span class="edit-btn js-edit-contact" data-key="${contact.id}">
+					</span>
+					<span class="delete-btn js-delete-contact" data-key="${contact.id}"></span>
+				</div>
+				<div class="contact-name">${contact.name}</div>
+			</div>
+			<div class="details-content">
+				<ul class="details-list">
+					<li class="detail-item">
+						<span class="detail-title">Mobile :</span>
+						<span class="detail">${contact.phone}</span>
+					</li>
+					<li class="detail-item">
+						<span class="detail-title">Email :</span>
+						<span class="detail-name">${contact.email}</span>
+					</li>
+				</ul>
+			</div>
+		`;
+		this.detailsTemplate.classList.add('active-template');
+		this.getUtilButtons();
+	}
+
+
+	hideTemplates() {
+		this.defaultTemplate.style.display = 'none';
+		for(let template of this.templates) {
+			template.classList.remove('active-template');
+		}
+	}
+
+	clearTemplate(template) {
+		template.innerHTML = '';
+	}
+
+	removeTemplate() {
+		for(let template of this.templates) {
+			template.classList.remove('active-template');
+			this.clearTemplate(template);
+		}
+		this.showDefault();
+	}
+
+	getUtilButtons() {
+		this.removeButton = document.querySelector('.js-remove-template');
+		this.editButton = document.querySelector('.js-edit-contact');
+		this.deleteButton = document.querySelector('.js-delete-contact');
+
+		this.attachListenerAndCallback(this.removeButton, 'click', this.removeTemplate);
+		// this.attachListenerAndCallback(this.editButton, this.editContact);
+		// this.attachListenerAndCallback(this.deleteButton, this.deleteContact);
+	}
+
+	
+
+}
+
+
 var App = new AddressBook('shadrach', contacts);
+App.view = new View();
 
 // let values = [...contactList.values()];
 // let keys = [...contactList.keys()];
@@ -121,4 +224,3 @@ var App = new AddressBook('shadrach', contacts);
 // 		console.log(name, phone);
 // 	}
 // }
-
