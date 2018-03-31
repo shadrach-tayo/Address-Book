6dbbfa50fe1b36abd5d63f2fc14554ee84c7dfe7
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2018-03-31 17:36:44 +0100

    Added new files and images

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+import contacts from './Data.js';
+
+for(let contact of contacts) {
+	console.log(contact.name);
+}
+
+const sidenavContainer = document.querySelector('.sidenav-container');
+const sidenav = document.querySelector('.sidenav');
+const sidenavToggle = document.querySelector('.sidenav-toggle');
+
+function toggleNav() {
+		sidenavContainer.classList.add('open');	
+		document.body.classList.add('no-scroll');
+}
+
+function closeNav() {
+	sidenavContainer.classList.remove('open');
+	document.body.classList.remove('no-scroll');
+}
+
+function blockClicks(e) {
+	e.stopPropagation();
+}
+
+sidenav.addEventListener('click', blockClicks);
+sidenavToggle.addEventListener('click', toggleNav);
+sidenavContainer.addEventListener('click', closeNav);
