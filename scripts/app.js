788eae5ecf6ad6320b48cb546858a72b81d294f3
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2018-04-12 01:05:56 +0100

    Commit minor syntax changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,5 @@
+import Contacts from './Data.js';
+
 class AddressBook {
 	constructor(name, contacts) {
 		// firstclass props
@@ -181,19 +183,19 @@ class AddressBook {
 }
 
 
-var contacts = [
-	{id: '16a', name: 'Shadrach Temitayo', phone: +2348146023912, email: 'shadrachtemitayo@gmail.com', avatarUrl: './assets/images/shadrach.jpg', backgroundUrl: '../images/shadrach-bg.jpeg'},
-	{id: '11a', name: 'Dan Abramov', phone: +23418933925, email: 'danabramov@gmail.com', avatarUrl: './assets/images/dan-abramov.jpg', backgroundUrl: '../images/dan-abramov.jpg'},
-	{id: '12a', name: 'kyle Simpson', phone: +23418933925, email: 'kylesimpson@gmail.com', avatarUrl: './assets/images/getify.jpg', backgroundUrl: '../images/getify.jpg'},
-	{id: '13a', name: 'Sophie Alpert', phone: +23418933925, email: 'sophiealpert@gmail.com', avatarUrl: './assets/images/sophie.jpg', backgroundUrl: '../images/sophie-bg.jpg'},
-	{id: '14a', name: 'Josh Pagley', phone: +23418933925, email: 'joshpagley@gmail.com', avatarUrl: './assets/images/jpegley.jpg', backgroundUrl: '../images/jpegley-bg.jpg'},
-	{id: '15a', name: 'Paul Lewis', phone: +23418933925, email: 'paul.lewis@gmail.com', avatarUrl: './assets/images/paul-lewis.jpg', backgroundUrl: '../images/paul-lewis-bg.jpg'},
-	{id: '17a', name: 'Msbrandy Morgan', phone: +23418933925, email: 'brandymorgan@gmail.com', avatarUrl: './assets/images/msbrandymorgan.jpg', backgroundUrl: '../images/msbrandymorgan-bg.jpg'},
-	{id: '18a', name: 'Nicholas Zakas', phone: +23418933925, email: 'nicholas@gmail.com', avatarUrl: './assets/images/zakas.jpg', backgroundUrl: '../images/zakas-bg.jpg'},
-	{id: '19a', name: 'Chris Sean', phone: +23418933925, email: 'chrissean@gmail.com', avatarUrl: './assets/images/chris-sean.jpg', backgroundUrl: '../images/chris-sean-bg.jpg'},
-	{id: '11b', name: 'Lydia Hallie', phone: +23418933925, email: 'lydiahallie@gmail.com', avatarUrl: './assets/images/lydia-hallie.jpg', backgroundUrl: '../images/lydia-hallie-bg.jpg'},
-	{id: '11c', name: 'Ryan Florence', phone: +23418933925, email: 'ryan@gmail.com', avatarUrl: './assets/images/ryan.jpg', backgroundUrl: '../images/ryan-bg.jpg'}
-];
+// var contacts = [
+// 	{id: '16a', name: 'Shadrach Temitayo', phone: +2348146023912, email: 'shadrachtemitayo@gmail.com', avatarUrl: './assets/images/shadrach.jpg', backgroundUrl: '../images/shadrach-bg.jpeg'},
+// 	{id: '11a', name: 'Dan Abramov', phone: +23418933925, email: 'danabramov@gmail.com', avatarUrl: './assets/images/dan-abramov.jpg', backgroundUrl: '../images/dan-abramov.jpg'},
+// 	{id: '12a', name: 'kyle Simpson', phone: +23418933925, email: 'kylesimpson@gmail.com', avatarUrl: './assets/images/getify.jpg', backgroundUrl: '../images/getify.jpg'},
+// 	{id: '13a', name: 'Sophie Alpert', phone: +23418933925, email: 'sophiealpert@gmail.com', avatarUrl: './assets/images/sophie.jpg', backgroundUrl: '../images/sophie-bg.jpg'},
+// 	{id: '14a', name: 'Josh Pagley', phone: +23418933925, email: 'joshpagley@gmail.com', avatarUrl: './assets/images/jpegley.jpg', backgroundUrl: '../images/jpegley-bg.jpg'},
+// 	{id: '15a', name: 'Paul Lewis', phone: +23418933925, email: 'paul.lewis@gmail.com', avatarUrl: './assets/images/paul-lewis.jpg', backgroundUrl: '../images/paul-lewis-bg.jpg'},
+// 	{id: '17a', name: 'Msbrandy Morgan', phone: +23418933925, email: 'brandymorgan@gmail.com', avatarUrl: './assets/images/msbrandymorgan.jpg', backgroundUrl: '../images/msbrandymorgan-bg.jpg'},
+// 	{id: '18a', name: 'Nicholas Zakas', phone: +23418933925, email: 'nicholas@gmail.com', avatarUrl: './assets/images/zakas.jpg', backgroundUrl: '../images/zakas-bg.jpg'},
+// 	{id: '19a', name: 'Chris Sean', phone: +23418933925, email: 'chrissean@gmail.com', avatarUrl: './assets/images/chris-sean.jpg', backgroundUrl: '../images/chris-sean-bg.jpg'},
+// 	{id: '11b', name: 'Lydia Hallie', phone: +23418933925, email: 'lydiahallie@gmail.com', avatarUrl: './assets/images/lydia-hallie.jpg', backgroundUrl: '../images/lydia-hallie-bg.jpg'},
+// 	{id: '11c', name: 'Ryan Florence', phone: +23418933925, email: 'ryan@gmail.com', avatarUrl: './assets/images/ryan.jpg', backgroundUrl: '../images/ryan-bg.jpg'}
+// ];
 
 
 class View {
@@ -500,7 +502,7 @@ class Form {
 
 }
 
-var App = new AddressBook('shadrach', contacts);
+var App = new AddressBook('shadrach', Contacts);
 App.view = new View();
 var form = new Form();
 
